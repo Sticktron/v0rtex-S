5d9752a050644aae7af6d67a40c9a100972f0a53
Sticktron <sticktron@hotmail.com>
2017-12-10 10:44:35 -0500

    Removed old sploit; changed output messages a bit. Works.

--- v0rtex/ViewController.m
+++ v0rtex/ViewController.m
@@ -37,35 +37,49 @@ kern_return_t v0rtex(task_t *tfp0, kptr_t *kslide);
 }
 
 - (IBAction)runSploitButton:(UIButton *)sender {
-    self.outputTextView.text = @"run sploit";
+    NSString *output = @"exploiting... \r";
+    self.outputTextView.text = output;
     
+    // Get TFP0
     
     task_t tfp0 = MACH_PORT_NULL;
     kptr_t kslide = 0;
     kern_return_t ret = v0rtex(&tfp0, &kslide);
     
-    self.outputTextView.text = [self.outputTextView.text stringByAppendingString:@"\r"];
-    self.outputTextView.text = [self.outputTextView.text stringByAppendingString:@"finished."];
-    self.outputTextView.text = [self.outputTextView.text stringByAppendingString:@"\r"];
-    self.outputTextView.text = [self.outputTextView.text stringByAppendingString:[NSString stringWithFormat:@"ret = %d", ret]];
-    if (ret == KERN_SUCCESS) {
-        self.outputTextView.text = [self.outputTextView.text stringByAppendingString:@" (KERN_SUCCESS)"];
+    if (ret != KERN_SUCCESS) {
+        output = [output stringByAppendingString:@"ERROR: failed to get tfp0 \r\r"];
+        self.outputTextView.text = output;
+        return;
     }
     
-    // XXX
-    if(ret == KERN_SUCCESS)
-    {
-        extern kern_return_t mach_vm_read_overwrite(vm_map_t target_task, mach_vm_address_t address, mach_vm_size_t size, mach_vm_address_t data, mach_vm_size_t *outsize);
-        uint32_t magic = 0;
-        mach_vm_size_t sz = sizeof(magic);
-        ret = mach_vm_read_overwrite(tfp0, 0xfffffff007004000 + kslide, sizeof(magic), (mach_vm_address_t)&magic, &sz);
-        LOG("mach_vm_read_overwrite: %x, %s", magic, mach_error_string(ret));
-        
-        FILE *f = fopen("/var/mobile/test.txt", "w");
-        LOG("file: %p", f);
-    }
-    // XXX
+    output = [output stringByAppendingString:@"got tfp0 \r"];
+    output = [output stringByAppendingString:@"writing test file... \r"];
+    self.outputTextView.text = output;
     
+    
+    // Write Test File
+    
+    extern kern_return_t mach_vm_read_overwrite(vm_map_t target_task, mach_vm_address_t address, mach_vm_size_t size, mach_vm_address_t data, mach_vm_size_t *outsize);
+    uint32_t magic = 0;
+    mach_vm_size_t sz = sizeof(magic);
+    ret = mach_vm_read_overwrite(tfp0, 0xfffffff007004000 + kslide, sizeof(magic), (mach_vm_address_t)&magic, &sz);
+    LOG("mach_vm_read_overwrite: %x, %s", magic, mach_error_string(ret));
+    
+    FILE *f = fopen("/var/mobile/test.txt", "w");
+    LOG("file: %p", f);
+    
+    if (f) {
+        output = [output stringByAppendingString:[NSString stringWithFormat:@"success! wrote test file (%p) \r", f]];
+    } else {
+        output = [output stringByAppendingString:@"ERROR: failed to write test file \r"];
+    }
+    self.outputTextView.text = output;
+    
+    
+    
+    // End
+    output = [output stringByAppendingString:@"end. \r\r"];
+    self.outputTextView.text = output;
 }
 
 @end

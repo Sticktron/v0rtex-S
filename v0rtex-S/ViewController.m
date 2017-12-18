b20d83fe5e60c727c98048aca0abd596929ff69b
Sticktron <sticktron@hotmail.com>
2017-12-18 07:03:36 -0500

    Dark style; more logging; fixed layout constraints

--- v0rtex-S/ViewController.m
+++ v0rtex-S/ViewController.m
@@ -8,7 +8,11 @@
 
 #import "ViewController.h"
 
-#include "v0rtex.h"
+#import "v0rtex.h"
+#import <sys/utsname.h>
+
+
+ViewController *controller;
 
 
 @interface ViewController ()
@@ -23,54 +27,73 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    self.sploitButton.layer.cornerRadius = 6;
+    self.sploitButton.layer.cornerRadius = 8;
+    
     self.outputView.layer.cornerRadius = 6;
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-- (IBAction)runSploitButton:(UIButton *)sender {
+    self.outputView.text = @"";
     
-    // Run v0rtex
     
-    self.outputView.text = [self.outputView.text stringByAppendingString:@"\n > running exploit... \n"];
-
-    task_t tfp0 = MACH_PORT_NULL;
-    kptr_t kslide = 0;
+    // get kernel info
+    struct utsname u = { 0 };
+    uname(&u);
+    [self log:[NSString stringWithFormat:@"%s \n\n", u.version]];
     
-    kern_return_t v0rtex(task_t *tfp0, kptr_t *kslide);
-    kern_return_t ret = v0rtex(&tfp0, &kslide);
-    
-    if (ret != KERN_SUCCESS) {
-        self.outputView.text = [self.outputView.text stringByAppendingString:@"ERROR: exploit failed \n"];
+    // already jailbroken?
+    if (strstr(u.version, "MarijuanARM")) {
+        self.sploitButton.enabled = NO;
+        [self.sploitButton setTitle:@"already jailbroken" forState:UIControlStateDisabled];
+        [self log:@"Already jailbroke god. \n\n"];
         return;
     }
     
-    self.outputView.text = [self.outputView.text stringByAppendingString:@"exploit succeeded! \n"];
+    // store a global ref to ourself
+    controller = self;
+    
+    [self log:@"Ready. \n"];
+}
+
+- (void)log:(NSString *)message {
+    if (message && self.outputView) {
+        self.outputView.text = [self.outputView.text stringByAppendingString:message];
+    }
+}
+
+- (IBAction)runSploit:(UIButton *)sender {
+    
+    [self log:@"> exploiting kernel... \n"];
+    
+    task_t tfp0 = MACH_PORT_NULL;
+    kptr_t kslide = 0;
+    
+    kern_return_t ret = v0rtex(&tfp0, &kslide);
+    if (ret != KERN_SUCCESS) {
+        [self log:@"ERROR: exploit failed \n\n"];
+        return;
+    }
+    [self log:[NSString stringWithFormat:@"SUCESS: got tfp0 -> %x \n", tfp0]];
+    [self log:[NSString stringWithFormat:@"slide = %llx \n", kslide]];
+    
+    self.sploitButton.enabled = NO;
     
     
-    // Write a test file
-    
-    self.outputView.text = [self.outputView.text stringByAppendingString:@"writing test file... \n"];
-    
+    // print kernel magic
     extern kern_return_t mach_vm_read_overwrite(vm_map_t target_task, mach_vm_address_t address, mach_vm_size_t size, mach_vm_address_t data, mach_vm_size_t *outsize);
     uint32_t magic = 0;
     mach_vm_size_t sz = sizeof(magic);
     ret = mach_vm_read_overwrite(tfp0, 0xfffffff007004000 + kslide, sizeof(magic), (mach_vm_address_t)&magic, &sz);
-    LOG("mach_vm_read_overwrite: %x, %s", magic, mach_error_string(ret));
+    //LOG("mach_vm_read_overwrite: %x, %s", magic, mach_error_string(ret));
+    [self log:[NSString stringWithFormat:@"found kernel magic: %x \n", magic]];
     
+    
+    // write a test file
+    [self log:@"> writing test file... \n"];
     FILE *f = fopen("/var/mobile/test.txt", "w");
-    LOG("file: %p", f);
+    //LOG("file: %p", f);
     if (f == 0) {
-        self.outputView.text = [self.outputView.text stringByAppendingString:@"ERROR: failed to write test file \n"];
+        [self log:@"ERROR: failed to write test file \n\n"];
         return;
     }
-    
-    self.outputView.text = [self.outputView.text stringByAppendingString:@"wrote test file! \n"];
-    self.outputView.text = [self.outputView.text stringByAppendingString:[NSString stringWithFormat:@"/var/mobile/test.txt (%p) \n", f]];
+    [self log:[NSString stringWithFormat:@"wrote /var/mobile/test.txt (%p) \n", f]];
     
     
     // Next steps ???
@@ -79,8 +102,12 @@
     
     
     // Done.
-    self.outputView.text = [self.outputView.text stringByAppendingString:@"\n"];
-    self.outputView.text = [self.outputView.text stringByAppendingString:@"done. \n"];
+    self.outputView.text = [self.outputView.text stringByAppendingString:@"finished. \n\n"];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
 }
 
 @end

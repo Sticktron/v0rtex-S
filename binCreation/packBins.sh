9cde83f843e522122dfb636a5afe33d14bbde0e5
Ben Sparkes <bensparkes8@gmail.com>
2017-12-22 00:19:10 +0000

    binpack stuff

--- binCreation/packBins.sh
+++ binCreation/packBins.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
-binCount=$(ls -1q ./bins | wc -l | sed -e 's/^[ `t]*//')
+currDir=$(dirname $0)
+
+echo "Found current dir: $currDir"
+
+binCount=$(ls -1q  $currDir/bins | wc -l | sed -e 's/^[ `t]*//')
 
 echo "Found $binCount bins"
 
 echo "Giving all bins correct permissions (755)..."
 
-for f in ./bins/*
+for file in $currDir/bins/*
 do
-  chmod 755 $f
+  chmod 755 $file
 done
 
 echo "Packing the binpack..."
 
-cd $PWD
+cd $currDir
 tar -cf bootstrap.tar ./bins/*
 
-echo "Packed $binCount bins from $PWD/bins into $PWD/bootstrap.tar!"
+echo "Packed $binCount bins from $currDir/bins into $currDir/bootstrap.tar!"
 
 echo "Please copy the bootstrap.tar file into the Xcode project!"

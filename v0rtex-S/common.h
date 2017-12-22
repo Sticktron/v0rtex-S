30dbd093dde820b2bd5bb30e8f22a053f819fee6
Sticktron <sticktron@hotmail.com>
2017-12-21 23:29:10 -0500

    turn off full logging

--- v0rtex-S/common.h
+++ v0rtex-S/common.h
@@ -4,8 +4,6 @@
 #include <stdint.h>             // uint*_t
 #include <Foundation/Foundation.h>
 
-//#define LOG(str, args...) do { NSLog(@str "\n", ##args); } while(0)
-
 #ifdef __LP64__
 #   define ADDR "0x%016llx"
     typedef uint64_t kptr_t;
@@ -14,19 +12,21 @@
     typedef uint32_t kptr_t;
 #endif
 
+#define LOG(str, args...) do { NSLog(@str "\n", ##args); } while(0)
+
 // Re-direct LOG macro to GUI
-#include "ViewController.h"
-extern id controller;
-#define LOG(str, args...) do { \
-    if (controller && [controller respondsToSelector:@selector(log:)]) { \
-        if ([NSThread isMainThread]) { \
-            [controller log:[NSString stringWithFormat:@str "\n", ##args]]; \
-        } else { \
-            [controller performSelectorOnMainThread:@selector(log:) withObject:[NSString stringWithFormat:@str "\n", ##args] waitUntilDone:NO]; \
-        } \
-    } else { \
-        NSLog(@str "\n", ##args); \
-    } \
-} while(0)
+//#include "ViewController.h"
+//extern id controller;
+//#define LOG(str, args...) do { \
+//    if (controller && [controller respondsToSelector:@selector(log:)]) { \
+//        if ([NSThread isMainThread]) { \
+//            [controller log:[NSString stringWithFormat:@str "\n", ##args]]; \
+//        } else { \
+//            [controller performSelectorOnMainThread:@selector(log:) withObject:[NSString stringWithFormat:@str "\n", ##args] waitUntilDone:NO]; \
+//        } \
+//    } else { \
+//        NSLog(@str "\n", ##args); \
+//    } \
+//} while(0)
 
 #endif

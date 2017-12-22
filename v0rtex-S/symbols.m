b8de5d457da2a4d4a0b97328e504651a35de98dd
Sticktron <sticktron@hotmail.com>
2017-12-21 23:30:55 -0500

    added symbols for 6S

--- v0rtex-S/symbols.m
+++ v0rtex-S/symbols.m
@@ -40,59 +40,58 @@ BOOL init_symbols()
     
     LOG("Device: %s", u.machine);
     LOG("Device Name: %s", u.nodename);
-    LOG("Device iOS Version: %@", ver);
+    LOG("iOS Version: %@", ver);
     
-    if (strcmp(u.machine, "iPhone9,3") == 0)
+    if (strcmp(u.machine, "iPhone9,3") == 0 && [ver isEqual:@"Version 10.3.1 (Build 14E304)"])
     {
-        if ([ver  isEqual: @"Version 10.3.1 (Build 14E304)"])
-        {
-            OFFSET_ZONE_MAP                             = 0xfffffff007590478;
-            OFFSET_KERNEL_MAP                           = 0xfffffff0075ec050;
-            OFFSET_KERNEL_TASK                          = 0xfffffff0075ec048;
-            OFFSET_REALHOST                             = 0xfffffff007572ba0;
-            OFFSET_BZERO                                = 0xfffffff0070c1f80;
-            OFFSET_BCOPY                                = 0xfffffff0070c1dc0;
-            OFFSET_COPYIN                               = 0xfffffff0071c6134;
-            OFFSET_COPYOUT                              = 0xfffffff0071c6414;
-            OFFSET_CHGPROCCNT                           = 0xfffffff007049e4b;
-            OFFSET_KAUTH_CRED_REF                       = 0xfffffff0073ada04;
-            OFFSET_IPC_PORT_ALLOC_SPECIAL               = 0xfffffff0070df05c;
-            OFFSET_IPC_KOBJECT_SET                      = 0xfffffff0070f22b4;
-            OFFSET_IPC_PORT_MAKE_SEND                   = 0xfffffff0070deb80;
-            OFFSET_IOSURFACEROOTUSERCLIENT_VTAB         = 0xfffffff006e4a238;
-            OFFSET_ROP_ADD_X0_X0_0x10                   = 0xfffffff0064ff0a8;
-            OFFSET_ROP_LDR_X0_X0_0x10                   = 0xfffffff0074cf02c;
-            OFFSET_ROOT_MOUNT_V_NODE                    = 0xfffffff0075ec0b0;
-        }
+        OFFSET_ZONE_MAP                             = 0xfffffff007590478;
+        OFFSET_KERNEL_MAP                           = 0xfffffff0075ec050;
+        OFFSET_KERNEL_TASK                          = 0xfffffff0075ec048;
+        OFFSET_REALHOST                             = 0xfffffff007572ba0;
+        OFFSET_BZERO                                = 0xfffffff0070c1f80;
+        OFFSET_BCOPY                                = 0xfffffff0070c1dc0;
+        OFFSET_COPYIN                               = 0xfffffff0071c6134;
+        OFFSET_COPYOUT                              = 0xfffffff0071c6414;
+        OFFSET_CHGPROCCNT                           = 0xfffffff007049e4b;
+        OFFSET_KAUTH_CRED_REF                       = 0xfffffff0073ada04;
+        OFFSET_IPC_PORT_ALLOC_SPECIAL               = 0xfffffff0070df05c;
+        OFFSET_IPC_KOBJECT_SET                      = 0xfffffff0070f22b4;
+        OFFSET_IPC_PORT_MAKE_SEND                   = 0xfffffff0070deb80;
+        OFFSET_IOSURFACEROOTUSERCLIENT_VTAB         = 0xfffffff006e4a238;
+        OFFSET_ROP_ADD_X0_X0_0x10                   = 0xfffffff0064ff0a8;
+        OFFSET_ROP_LDR_X0_X0_0x10                   = 0xfffffff0074cf02c;
+        OFFSET_ROOT_MOUNT_V_NODE                    = 0xfffffff0075ec0b0;
+        LOG("loaded offsets for iPhone9,3 on 10.3.1");
     }
     
-    else if (strcmp(u.machine, "iPhone8,1"))
+    // iPhone 6S - 10.3.2
+    else if (strcmp(u.machine, "iPhone8,1") == 0 && [ver isEqual:@"Version 10.3.2 (Build 14F89)"])
     {
-        if ([ver isEqual: @"Version 10.3.2 (Build 14F89)"])
-        {
-            OFFSET_ZONE_MAP                             = 0xfffffff007548478;
-            OFFSET_KERNEL_MAP                           = 0xfffffff0075a4050;
-            OFFSET_KERNEL_TASK                          = 0xfffffff0075a4048;
-            OFFSET_REALHOST                             = 0xfffffff00752aba0;
-            OFFSET_BZERO                                = 0xfffffff007081f80;
-            OFFSET_BCOPY                                = 0xfffffff007081dc0;
-            OFFSET_COPYIN                               = 0xfffffff0071806f4;
-            OFFSET_COPYOUT                              = 0xfffffff0071808e8;
-            OFFSET_IPC_PORT_ALLOC_SPECIAL               = 0xfffffff007099e94;
-            OFFSET_IPC_KOBJECT_SET                      = 0xfffffff0070ad16c;
-            OFFSET_IPC_PORT_MAKE_SEND                   = 0xfffffff0070999b8;
-            OFFSET_IOSURFACEROOTUSERCLIENT_VTAB         = 0xfffffff006e7c9f8;
-            OFFSET_ROP_ADD_X0_X0_0x10                   = 0xfffffff006b916b8;
-            OFFSET_ROOT_MOUNT_V_NODE                    = 0xfffffff0075ec0b0;
-        }
+        OFFSET_ZONE_MAP                             = 0xfffffff007548478; /* "zone_init: kmem_suballoc failed" */
+        OFFSET_KERNEL_MAP                           = 0xfffffff0075a4050;
+        OFFSET_KERNEL_TASK                          = 0xfffffff0075a4048;
+        OFFSET_REALHOST                             = 0xfffffff00752aba0; /* host_priv_self */
+        OFFSET_BZERO                                = 0xfffffff007081f80;
+        OFFSET_BCOPY                                = 0xfffffff007081dc0;
+        OFFSET_COPYIN                               = 0xfffffff0071806f4;
+        OFFSET_COPYOUT                              = 0xfffffff0071808e8;
+        OFFSET_CHGPROCCNT                           = 0xfffffff007049df1;
+        OFFSET_KAUTH_CRED_REF                       = 0xfffffff007367cf4;
+        OFFSET_IPC_PORT_ALLOC_SPECIAL               = 0xfffffff007099e94; /* convert_task_suspension_token_to_port */
+        OFFSET_IPC_KOBJECT_SET                      = 0xfffffff0070ad16c; /* convert_task_suspension_token_to_port */
+        OFFSET_IPC_PORT_MAKE_SEND                   = 0xfffffff0070999b8; /* "ipc_host_init" */
+        OFFSET_IOSURFACEROOTUSERCLIENT_VTAB         = 0xfffffff006e7c9f8;
+        OFFSET_ROP_ADD_X0_X0_0x10                   = 0xfffffff0064b1398;
+        OFFSET_ROP_LDR_X0_X0_0x10                   = 0xfffffff0074c31d4;
+        OFFSET_ROOT_MOUNT_V_NODE                    = 0xfffffff0075a40b0;
+        LOG("loaded offsets for iPhone 6 on 10.3.2");
     }
     
     else
     {
         LOG("Device not supported.");
-        return FALSE;
+        return NO;
     }
     
-    return TRUE;
+    return YES;
 }
-

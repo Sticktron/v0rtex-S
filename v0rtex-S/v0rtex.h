97f1f068c7a03cdba140b166cdfe98cc40e7f3a9
Cryptiiiic <liamwqs@gmail.com>
2017-12-27 21:29:29 -0800

    Potential AMFI Patch

--- v0rtex-S/v0rtex.h
+++ v0rtex-S/v0rtex.h
@@ -18,6 +18,21 @@
 #define OFFSET_REALHOST_SPECIAL                     0x10
 #define OFFSET_IOUSERCLIENT_IPC                     0x9c
 #define OFFSET_VTAB_GET_EXTERNAL_TRAP_FOR_INDEX     0x5b8
+#define OFFSET_P_CSFLAGS                            0x2a8
+#define OFFSET_P_CPUTYPE                            0x2c0
+#define OFFSET_P_CPU_SUBTYPE                        0x2c4
+#define OFFSET_P_TEXTVP                             0x248
+#define OFFSET_P_TEXTOFF                            0x248
+#define OFFSET_V_TYPE                               0x70
+#define OFFSET_V_UBCINFO                            0x78
+#define OFFSET_UBCINFO_CSBLOBS                    0x50
+#define OFFSET_CSB_CPUTYPE                          0x8
+#define OFFSET_CSB_FLAGS                            0x12
+#define OFFSET_CSB_BASE                             0x16
+#define OFFSET_CSB_ENTITLEMENTS                     0x98
+#define OFFSET_CSB_SIGNER_TYPE                       0xA0
+#define OFFSET_CSB_PLATFORM_BINARY                  0xA4
+#define OFFSET_CSB_PLATFORM_PATH                    0xA8
 
 kern_return_t v0rtex(task_t *tfp0, kptr_t *kslide, kptr_t *kernucred, kptr_t *selfproc);
 

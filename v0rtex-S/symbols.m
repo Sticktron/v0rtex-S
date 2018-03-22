16c035aa6174f3259fd6cf070784ad3e99244aaa
unmec <unmec@github>
2018-03-22 11:53:48 +0800

    Add iPhone 7 10.3.3 offsets

--- v0rtex-S/symbols.m
+++ v0rtex-S/symbols.m
@@ -63,6 +63,30 @@ BOOL init_symbols()
         OFFSET_ROOT_MOUNT_V_NODE                    = 0xfffffff0075ec0b0;
         LOG("loaded offsets for iPhone 7 on 10.3.1");
     }
+
+    // iPhone 7 Global, 9,1, A1660 - 10.3.3
+    else if (strcmp(u.machine, "iPhone9,1") == 0 && [ver isEqual:@"Version 10.3.3 (Build 14G60)"])
+    {
+        OFFSET_ZONE_MAP                             = 0xfffffff007590478;
+        OFFSET_KERNEL_MAP                           = 0xfffffff0075ec050;
+        OFFSET_KERNEL_TASK                          = 0xfffffff0075ec048;
+        OFFSET_REALHOST                             = 0xfffffff007572ba0;
+        OFFSET_BZERO                                = 0xfffffff0070c1f80;
+        OFFSET_BCOPY                                = 0xfffffff0070c1dc0;
+        OFFSET_COPYIN                               = 0xfffffff0071c5db4;
+        OFFSET_COPYOUT                              = 0xfffffff0071c6094;
+        OFFSET_CHGPROCCNT                           = 0xfffffff0073d38e4;
+        OFFSET_KAUTH_CRED_REF                       = 0xfffffff0073adc68;
+        OFFSET_IPC_PORT_ALLOC_SPECIAL               = 0xfffffff0070deff4;
+        OFFSET_IPC_KOBJECT_SET                      = 0xfffffff0070f22cc;
+        OFFSET_IPC_PORT_MAKE_SEND                   = 0xfffffff0070deb18;
+        OFFSET_IOSURFACEROOTUSERCLIENT_VTAB         = 0xfffffff006e4a238;
+        OFFSET_ROP_ADD_X0_X0_0x10                   = 0xfffffff0063c5398;
+        OFFSET_ROP_LDR_X0_X0_0x10                   = 0xfffffff006310a84;
+        OFFSET_ROOT_MOUNT_V_NODE                    = 0xfffffff0075ec0b0;
+        LOG("loaded offsets for iPhone 7 Global on 10.3.3");
+    }    
+
     
     // iPhone 6S - 10.3.2
     else if (strcmp(u.machine, "iPhone8,1") == 0 && [ver isEqual:@"Version 10.3.2 (Build 14F89)"])
